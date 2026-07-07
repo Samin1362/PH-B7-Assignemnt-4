486768774831e6a452cf2fdacb652183b30c13fc
samin1362 <saminisrak1991@gmail.com>
2026-07-07 13:37:59 +0600

    feat: add structured error handling and response utilities

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import cookieParser from "cookie-parser";
 import express, {Application, Request, Response} from "express";
 import cors from "cors";
 import config from "./config";
+import globalErrorHandler from "./middlewares/globalErrorHandler";
+import notFound from "./middlewares/notFound";
 
 const app : Application = express();
 
@@ -17,5 +19,8 @@ app.use(cookieParser());
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
+
+app.use(notFound);
+app.use(globalErrorHandler);
 
 export default app;
